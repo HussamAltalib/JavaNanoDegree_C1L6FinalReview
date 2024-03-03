8784947de1998e5dd556deb8c1b26bf62f986421
HussamAltalib <hussamaltalib@outlook.com>
2024-03-03 14:53:41 +0300

    course solution

--- src/main/java/com/udacity/jwdnd/c1/review/model/ChatMessage.java
+++ src/main/java/com/udacity/jwdnd/c1/review/model/ChatMessage.java
@@ -40,4 +40,7 @@ public class ChatMessage {
     public void setMessageText(String messageText) {
         this.messageText = messageText;
     }
+    public String getMessageText() {
+        return messageText;
+    }
 }
